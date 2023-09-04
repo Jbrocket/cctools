d6a67eeaca9ef296624b515e254457097b4958b1
Jonathan Brockett <jbrocket@cclws05.cse.nd.edu>
2023-09-04 16:10:36 -0400

    micromamba used to install instead of conda

--- packaging/build-conda/setup.sh
+++ packaging/build-conda/setup.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
-conda install -y  -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+~/bin/micromamba install -y  -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
+
