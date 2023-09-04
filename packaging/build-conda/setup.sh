f19fb0c4b1a1dc8d8feb104c537be71f4e4622cc
Jonathan Brockett <jbrocket@cclws05.cse.nd.edu>
2023-09-04 17:28:58 -0400

    file shenanigans

--- packaging/build-conda/setup.sh
+++ packaging/build-conda/setup.sh
@@ -1,3 +1,5 @@
 #! /bin/bash
 
+curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
 conda install -y  -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
