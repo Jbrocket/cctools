20ecef8195f74cff137a6f3e53a9b54948aa556b
Jonathan Brockett <jbrocket@cclws05.cse.nd.edu>
2023-09-06 15:52:27 -0400

    making progress2"

--- packaging/build-conda/setup.sh
+++ packaging/build-conda/setup.sh
@@ -4,6 +4,6 @@ conda update -n base conda
 conda --version
 
 conda install -n base conda-libmamba-solver
-conda config --set solver libmamba
+#conda config --set solver libmamba
 
 conda install -y  -c conda-forge --strict-channel-priority python=3 gcc_linux-64 gxx_linux-64 gdb m4 perl swig make zlib libopenssl-static openssl conda-pack cloudpickle packaging
